66e1e82fa28d222441d345306f247a90de9f4683
poruruba <44470781+poruruba@users.noreply.github.com>
2019-07-28 09:54:42 +0900

    Update start.js

--- public/login/js/start.js
+++ public/login/js/start.js
@@ -1,11 +1,11 @@
 const base_url = 'http://localhost:10080';
-
+const page_url = "http://localhost:10080';
 var vue_options = {
     el: "#top",
     data: {
         progress_title: '',
 
-        redirect_uri: './redirect.html',
+        redirect_uri: page_url + './redirect.html',
         authorize_endpoint: base_url + '/oauth2/authorize',
         authorize_direct_endpoint: base_url + '/oauth2/authorize-direct'
     },
