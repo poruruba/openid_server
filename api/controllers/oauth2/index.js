6dc02ee2937af7fd5562c36bade28274836417db
poruruba <44470781+poruruba@users.noreply.github.com>
2020-01-04 15:22:08 +0900

    Update index.js

--- api/controllers/oauth2/index.js
+++ api/controllers/oauth2/index.js
@@ -2,7 +2,7 @@
 
 const base_url = process.env.BASE_URL || 'http://localhost:10080';
 const page_url = process.env.PAGE_URL || 'http://localhost:10080';
-const issuer = process.env.ISSUER || 'https://localhost';
+const issuer = process.env.ISSUER || 'http://localhost:10080';
 
 const login_url = process.env.LOGIN_URL || (page_url + '/login/login.html');
 const keyid = process.env.KEYID || 'testkeyid';
@@ -15,8 +15,9 @@ var Redirect = require(HELPER_BASE + 'redirect');
 var fs = require('fs');
 var tojwks = require('rsa-pem-to-jwk');
 var jwt = require('jsonwebtoken');
-const { URL, URLSearchParams } = require('url');
+const { URLSearchParams } = require('url');
 var jwt_decode = require('jwt-decode');
+//const base64url = require('base64url');
 
 var jwkjson = null;
 const JWKS_BASE = process.env.JWKS_BASE || './api/controllers/oauth2/';
@@ -94,7 +95,7 @@ function make_tokens(client_id, userid, scope, refresh = true){
         subject: userid,
         keyid: keyid,
     });
-	
+
     var tokens = {
         access_token : access_token,
         id_token : id_token,
@@ -109,20 +110,22 @@ function make_tokens(client_id, userid, scope, refresh = true){
     return tokens;
 }
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
     if( event.path == '/oauth2/token'){
-	// Lambda＋API Gatewayの場合はこちら
+        // Lambda＋API Gatewayの場合はこちら
         // var params = new URLSearchParams(event.body);
         // swagger_nodeの場合はこちら
-	var params = Object.entries(JSON.parse(event.body)).reduce((l,[k,v])=>l.set(k,v), new Map());
+        var params = Object.entries(JSON.parse(event.body)).reduce((l,[k,v])=>l.set(k,v), new Map());
 
         var grant_type = params.get('grant_type');
         if( grant_type == 'authorization_code' || grant_type == "refresh_token"){
             var code;
             if( grant_type == "refresh_token" )
-                code = Buffer.from(params.get('refresh_token'), 'hex').toString('ascii');
+                code = params.get('refresh_token');
             else
-                code = Buffer.from(params.get('code'), 'hex').toString('ascii');
+                code = params.get('code');
+            code = Buffer.from(code, 'hex').toString('ascii');
+//            code = base64url.decode(code);
             var code_list = code.split(':');
             
             var client_id = code_list[0];
@@ -140,9 +143,19 @@ exports.handler = (event, context, callback) => {
 
             callback(null, new Response(tokens));
         }
+    }else if( event.path == '/oauth2/authorize' ){
+        var { client_id, redirect_uri, response_type, scope, state } = event.queryStringParameters;
+
+        var url = login_url + '?client_id=' + client_id + '&redirect_uri=' + encodeURIComponent(redirect_uri) + '&response_type=' + response_type;
+        if( scope )
+            url += '&scope=' + encodeURIComponent(scope);
+        if( state )
+            url += '&state=' + encodeURIComponent(state);
+
+        callback(null, new Redirect(url));
     }else if( event.path == '/oauth2/authorize-process' ){
         var { client_id, userid, password, redirect_uri, response_type, scope, state } = event.queryStringParameters;
-
+    
         if( response_type == 'token'){
             var tokens = make_tokens(client_id, userid, scope);
 
@@ -153,7 +166,9 @@ exports.handler = (event, context, callback) => {
 
             callback(null, new Redirect(url));
         }else if( response_type == 'code' ){
-            var code = Buffer.from(client_id + ':' + userid + ':' + scope, 'ascii').toString('hex');
+            var code = client_id + ':' + userid + ':' + scope;
+            code = Buffer.from(code, 'ascii').toString('hex');
+//            code = base64url.encode(code);
 
             var url = redirect_uri + '?code=' + code;
             if( state )
@@ -161,30 +176,22 @@ exports.handler = (event, context, callback) => {
 
             callback(null, new Redirect(url));
         }
-    }else if( event.path == '/oauth2/authorize' ){
-        var { client_id, redirect_uri, response_type, scope, state } = event.queryStringParameters;
-
-        var url = login_url + '?client_id=' + client_id + '&redirect_uri=' + encodeURIComponent(redirect_uri) + '&response_type=' + response_type;
-        if( scope )
-            url += '&scope=' + encodeURIComponent(scope);
-	if( state )
-            url += '&state=' + encodeURIComponent(state);
-
-        callback(null, new Redirect(url));
     }else if( event.path == '/oauth2/userInfo'){
         if( event.httpMethod == 'GET'){
 //            var token = jwt_decode(event.headers.authorization);
             var token = event.requestContext.authorizer.claims;
+
             callback(null, new Response(token));
         }else if( event.httpMethod == 'POST' ){
-            var body = JSON.parse(event.body);
-            var token;
-            if( !body.access_token ){
-                token = jwt_decode(event.headers.authorization);
-            }else{
-                token = jwt_decode(body.access_token);
-            }
-            callback(null, new Response(token));
+            // Lambda＋API Gatewayの場合はこちら
+            // var params = new URLSearchParams(event.body);
+            // swagger_nodeの場合はこちら
+            var params = Object.entries(JSON.parse(event.body)).reduce((l,[k,v])=>l.set(k,v), new Map());
+            var access_token = params.get('access_token');
+            if( !access_token )
+                access_token = event.headers.authorization;
+
+            callback(null, new Response(jwt_decode(access_token)));
         }
     }else if( event.path == '/.well-known/jwks.json'){
         if( jwkjson == null ){
@@ -211,7 +218,7 @@ exports.handler = (event, context, callback) => {
             scopes_supported: [
                 "openid",
                 "profile",
-		"email"
+                "email"
             ],
             subject_types_supported: [
                 "public"
